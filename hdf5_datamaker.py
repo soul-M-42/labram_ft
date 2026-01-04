c62c3ffe592458e15fe2143edd903457a8db97f7
soul-M-42 <zhangqingzhu2002@gmail.com>
2026-01-04 17:08:14 +0800

    updated hdf5_datamaker and readme

--- hdf5_datamaker.py
+++ hdf5_datamaker.py
@@ -7,10 +7,10 @@ from collections import defaultdict
 import h5py
 from pathlib import Path
 
-from scipy.interpolate import interp1d
-
 print(np.__version__)
+# 1.24.4
 print(mne.__version__)
+# 0.23.0
 
 
 
@@ -19,7 +19,7 @@ dataset_name = 'SEEDV'
 target_sfreq = 200
 sample_T = 2.0 # EEG sample window timelength in s
 sample_stride = 2.0 # # EEG sample window stride in s
-n_class = 5
+label_dim = 5
 input_dir = "C:\EEG_data\EEG_raw_cnt"
 output_dir = f"V:\daily_eeg_emotion\Data\SEED-V\SEEDV_hdf5_T={sample_T}s_stride={sample_stride}"
 sample_samples = int(sample_T * target_sfreq)
@@ -39,13 +39,6 @@ end_seconds = np.array([
     [321, 418, 643, 764, 877, 1147, 1284, 1418, 1679, 1996, 2275, 2425, 2664, 2857, 3066]
 ])
 
-# ======================= 45 个 trial 标签 =======================
-trial_labels = np.array([
-    4, 1, 3, 2, 0, 4, 1, 3, 2, 0, 4, 1, 3, 2, 0,
-    2, 1, 3, 0, 4, 2, 1, 3, 0, 4, 2, 1, 3, 0, 4,
-    2, 1, 3, 0, 4, 2, 1, 3, 0, 4, 2, 1, 3, 0, 4
-])
-
 # ======================= 文件匹配 =======================
 pattern = re.compile(r"(\d+)_(\d)_.*\.cnt")
 subject_files = defaultdict(dict)
@@ -55,6 +48,43 @@ for f in os.listdir(input_dir):
     if m:
         sub, sess = m.groups()
         subject_files[sub][int(sess)] = os.path.join(input_dir, f)
+
+n_sub = len(subject_files)
+
+# ======================= 45 个 trial 标签 =======================
+
+# Case 1. If your label is emotion class for each video:
+trial_labels = np.array([
+    4, 1, 3, 2, 0, 4, 1, 3, 2, 0, 4, 1, 3, 2, 0,
+    2, 1, 3, 0, 4, 2, 1, 3, 0, 4, 2, 1, 3, 0, 4,
+    2, 1, 3, 0, 4, 2, 1, 3, 0, 4, 2, 1, 3, 0, 4
+])
+trial_labels_onehot = (trial_labels.reshape(-1, 1) == np.arange(label_dim)).astype(int)
+all_labels = np.tile(trial_labels_onehot, (n_sub, 1, 1))
+print(all_labels.shape)
+
+# Case 2. If your label is multi-dim dynamic float:
+# all_labels = np.zeros((n_sub, n_vid, label_dim, T))
+
+# sub_trial_label = all_labels[sub][i_trial]
+# all_labels[sub][i_trial] is [label_dim, T]
+# T can be attribute and it will be resampled to match n_eeg_segment 
+
+
+def stretch_axis(arr, T_prime):
+    is_1d = arr.ndim == 1
+    if is_1d:
+        arr = arr[np.newaxis, :] # 变为 [1, k]
+    
+    T = arr.shape[-1]
+    x_old = np.arange(T)
+    x_new = np.linspace(0, T - 1, T_prime)
+    
+    # 对每一行执行插值
+    res = np.apply_along_axis(lambda y: np.interp(x_new, x_old, y), axis=-1, arr=arr)
+    
+    return res.flatten() if is_1d else res
+
 
 # ======================= 主处理 =======================
 ch_names = None
@@ -90,12 +120,6 @@ with h5py.File(data_save_path, 'w') as f:
         raw_all = mne.concatenate_raws(raws)
         data_all = raw_all.get_data()   # (C, T)
 
-        # data_all = linear_resample(
-        #     data_all,
-        #     orig_sfreq=sfreq,
-        #     target_sfreq=target_sfreq
-        # )
-
         # ======================= 只保留 trial 片段 =======================
         trial_segments = []
 
@@ -120,6 +144,9 @@ with h5py.File(data_save_path, 'w') as f:
             print(f'sub {sub} trial {i_trial}: {data_trial.shape}')
             vid_grp = sub_group.create_group(f"vid{i_trial}")
             n_samples = data_trial.shape[-1]
+            sub_trial_label = all_labels[sub][i_trial]
+            n_slices = (n_samples - sample_samples + stride_samples) // stride_samples
+            sub_trial_label = stretch_axis(sub_trial_label, n_slices)
             for i_slice, start in enumerate(range(0, n_samples - sample_samples + 1, stride_samples)):
                 end = start + sample_samples
                 slice_data = data_trial[:, start:end]
@@ -127,12 +154,6 @@ with h5py.File(data_save_path, 'w') as f:
                 dset = slice_grp.create_dataset('eeg', data=slice_data)
                 dset.attrs['chOrder'] = ch_names
                 dset.attrs['rsFreq'] = target_sfreq
-                # Label of the (i_slice)th eeg in video i_trial.
-                # Edit here if your label within video is dynamic. 
-
-                # class to onehot
-                label = np.zeros((n_class, 1))
-                label[int(trial_labels[i_trial])] = 1
-                dset.attrs['label'] = label
+                dset.attrs['label'] = sub_trial_label[i_slice]
                 dset.attrs['video_id'] = i_trial
                 dset.attrs['start_sample'] = start  # 可选：记录切片在原数据中的起始位置
